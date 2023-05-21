e7af72131f58fdfd4d4a37da563c780d84f609e5
zhaozhiwei <347061329@qq.com>
2023-05-21 16:31:44 +0800

    系统初始化脚本增加接口测试菜单

--- z-module-system/src/main/resources/liquibase/system/changelog/00001_create_zzw.sql
+++ z-module-system/src/main/resources/liquibase/system/changelog/00001_create_zzw.sql
@@ -14,3 +14,14 @@ VALUES(31, NULL, '2022-07-28 01:40:21', NULL, '2022-07-28 01:40:21', NULL, 1, 'e
 INSERT INTO sys_menu
 (id, created_by, created_date, last_modified_by, last_modified_date, config, enabled, icon_cls, keep_alive, name, order_num, parent_id, require_auth, url, component)
 VALUES(32, NULL, '2022-07-28 01:45:57', NULL, '2022-07-28 01:45:57', NULL, 1, NULL, NULL, '代码生成', 5, 31, NULL, 'code/generator', 'views/CodeGenerator/Index');
+
+delete from sys_menu where id in (45, 46);
+
+INSERT INTO sys_menu
+(id, created_by, created_date, last_modified_by, last_modified_date, config, enabled, icon_cls, keep_alive, name, order_num, parent_id, require_auth, url, component)
+VALUES(45, NULL, '2023-05-21 00:17:29', NULL, '2023-05-21 00:17:29', '', 1, '', NULL, '接口测试swagger', 1, 31, NULL, 'swagger', 'views/system/Swagger/Index');
+INSERT INTO money_making_machine_plus.sys_menu
+(id, created_by, created_date, last_modified_by, last_modified_date, config, enabled, icon_cls, keep_alive, name, order_num, parent_id, require_auth, url, component)
+VALUES(46, NULL, '2023-05-21 15:43:34', NULL, '2023-05-21 15:43:34', '', 1, '', NULL, '接口测试doc', 1, 31, NULL, 'knife4j-doc', 'views/system/Knife4jDoc/Index');
+
+alter table sys_menu auto_increment=50;
