e0a7976515866f5bff3958d8a695c78d34b3e168
zhaozhiwei <347061329@qq.com>
2023-05-29 23:55:53 +0800

    fix: 积木图片报表无法加载, 积木报表无法保存bug

--- z-module-report/src/main/java/com/z/module/report/config/ReportWebConfiger.java
+++ z-module-report/src/main/java/com/z/module/report/config/ReportWebConfiger.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class ReportWebConfiger implements WebMvcConfigurer {
 
@@ -25,9 +28,10 @@ public class ReportWebConfiger implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 进入预览, 设计, 列表页面时候进行token认证, 有的跳转没有token, 不能直接/jmreport/**
-        registry.addInterceptor(tokenIntercepter).addPathPatterns("/jmreport/view/**", "/jmreport/index/**", "/jmreport/list");
-        // 需要带入事务的url都配置下, 最好搞到文件里
-        registry.addInterceptor(transactionalInterceptor).addPathPatterns("/jmreport/delete");
+        final List<String> uiPathList = Arrays.asList("/jmreport/view/**", "/jmreport/index/**", "/jmreport/list");
+        registry.addInterceptor(tokenIntercepter).addPathPatterns(uiPathList);
+        // 需要带入事务的url, 省事点就全部, 命名不规范全配置太坑
+        registry.addInterceptor(transactionalInterceptor).addPathPatterns("/jmreport/**").excludePathPatterns(uiPathList);
     }
 
 }
