a1ddd79a2ab0f05ea6df543c8fbc86d5fef48415
zhaozhiwei <347061329@qq.com>
2023-06-03 00:00:58 +0800

    fix: 审计bug不生效bug, 增加注解处理

--- z-framework/z-common/src/main/java/com/z/framework/common/domain/AbstractAuditingEntity.java
+++ z-framework/z-common/src/main/java/com/z/framework/common/domain/AbstractAuditingEntity.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -17,10 +18,15 @@ import java.util.concurrent.TimeUnit;
  * <p>
  * 属性继承，必须在父类增加
  * Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+ * 如何使用jpa审计?
+ * 1. 实体类上添加 @EntityListeners(AuditingEntityListener.class)
+ * 2. 在需要的字段上加上 @CreatedDate、@CreatedBy、@LastModifiedDate、@LastModifiedBy 等注解。
+ * 3. 在Xxx Application 启动类上添加 @EnableJpaAuditing
  */
 @MappedSuperclass
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Data
+@EntityListeners(AuditingEntityListener.class) // 使用审计必须有这个注解
 public abstract class AbstractAuditingEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
