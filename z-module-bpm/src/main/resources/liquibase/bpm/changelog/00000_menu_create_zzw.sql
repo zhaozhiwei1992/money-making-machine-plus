f164784330179e89a068481140917f8183738d03
zhaozhiwei <zhaozhiwei@szlongtu.com>
2023-06-02 14:10:19 +0800

    fix: 工作流整合一些问题处理

--- z-module-bpm/src/main/resources/liquibase/bpm/changelog/00000_menu_create_zzw.sql
+++ z-module-bpm/src/main/resources/liquibase/bpm/changelog/00000_menu_create_zzw.sql
@@ -11,9 +11,9 @@ INSERT INTO sys_menu
 (id, created_by, created_date, last_modified_by, last_modified_date, config, enabled, icon_cls, keep_alive, name, order_num, parent_id, require_auth, url, component)
 VALUES(52, NULL, '2022-07-28 01:40:21', NULL, '2022-07-28 01:40:21', NULL, 1, 'ep:management', NULL, '流程定义', 15, 51, NULL, 'model', 'views/bpm/Model/Index');
 
-INSERT INTO sys_menu
-(id, created_by, created_date, last_modified_by, last_modified_date, config, enabled, icon_cls, keep_alive, name, order_num, parent_id, require_auth, url, component)
-VALUES(53, NULL, '2022-07-28 01:40:21', NULL, '2022-07-28 01:40:21', NULL, 1, 'ep:management', NULL, '表单配置', 15, 51, NULL, 'form', 'views/bpm/Form/Index');
+--INSERT INTO sys_menu
+--(id, created_by, created_date, last_modified_by, last_modified_date, config, enabled, icon_cls, keep_alive, name, order_num, parent_id, require_auth, url, component)
+--VALUES(53, NULL, '2022-07-28 01:40:21', NULL, '2022-07-28 01:40:21', NULL, 1, 'ep:management', NULL, '表单配置', 15, 51, NULL, 'form', 'views/bpm/Form/Index');
 
 INSERT INTO sys_menu
 (id, created_by, created_date, last_modified_by, last_modified_date, config, enabled, icon_cls, keep_alive, name, order_num, parent_id, require_auth, url, component)
