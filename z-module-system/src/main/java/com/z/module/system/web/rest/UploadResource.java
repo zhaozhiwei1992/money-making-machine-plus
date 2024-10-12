4c425b1b14dc88118eb06e907f34d616f6260077
zhaozhiwei <347061329@qq.com>
2024-10-12 14:47:26 +0800

    文件上传

--- z-module-system/src/main/java/com/z/module/system/web/rest/UploadResource.java
+++ z-module-system/src/main/java/com/z/module/system/web/rest/UploadResource.java
@@ -8,14 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 @Tag(name = "文件上传API")
 @RestController
